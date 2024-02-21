eb29a2cd033014b49b477a7a987d01c0943cf37a
ghkddlscks19 <84233046+ghkddlscks19@users.noreply.github.com>
2024-02-22 02:35:39 +0900

    [Silver III] Title: 2×n 타일링, Time: 212 ms, Memory: 17776 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int dp[] = new int[1001];
+
+        dp[1] = 1;
+        dp[2] = 2;
+
+
+        for (int i = 3; i <= n; i++) {
+            dp[i] = (dp[i - 2] + dp[i - 1]) % 10007;
+        }
+
+        System.out.println(dp[n]);
+    }
+}
