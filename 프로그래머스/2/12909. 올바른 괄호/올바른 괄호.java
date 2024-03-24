e01118042510660238c757e62fe319126841c457
ghkddlscks19 <84233046+ghkddlscks19@users.noreply.github.com>
2024-03-24 17:28:15 +0900

    [level 2] Title: 올바른 괄호, Time: 17.32 ms, Memory: 53.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,17 @@
+import java.util.Stack;
+class Solution {
+    boolean solution(String s) {
+        //스택 생성
+        Stack<Character> stack = new Stack<>();
+        
+        for(char c : s.toCharArray()){
+            if(c == '(') stack.push(c);
+            else{
+                if(stack.isEmpty()) return false;
+                stack.pop();
+            }
+        }
+        
+        return stack.isEmpty();
+    }
+}
