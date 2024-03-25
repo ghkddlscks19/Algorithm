0ecfa6968e26a23fd8c318f4207813fd1f2ac2b2
ghkddlscks19 <84233046+ghkddlscks19@users.noreply.github.com>
2024-03-26 00:42:37 +0900

    [level 2] Title: 주식가격, Time: 21.49 ms, Memory: 67.6 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,12 +1,29 @@
+import java.util.Stack;
 class Solution {
     public int[] solution(int[] prices) {
-        int[] answer = new int[prices.length];
-        for(int i =0; i<prices.length; i++){
-            for(int j = i+1; j<prices.length; j++){
-                answer[i]++;
-                if(prices[i] > prices[j]) break;
+        int n = prices.length;
+        int[] answer = new int[n];
+        Stack<Integer> stack = new Stack<>();
+        
+        //0번째 원소는 비교할게 없으니 일단 push
+        stack.push(0);
+        
+        for(int i =1; i<n; i++){
+            //비어있지 않으면서 가격이 떨어졌을 때
+            while(!stack.isEmpty() && prices[stack.peek()] > prices[i]){
+                int pop = stack.pop();
+                answer[pop] = i-pop;
             }
+            stack.push(i);
         }
+        
+        //스택에 남아있는 것들로 주식가격 계산
+        while(!stack.isEmpty()){
+            int pop = stack.pop();
+            //여기 개념을 몰랐음.
+            answer[pop] = n-pop-1;
+        }
+        
         return answer;
     }
 }
