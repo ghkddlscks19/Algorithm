2e9125600393b74f13208b6cabb58b76911817ce
ghkddlscks19 <84233046+ghkddlscks19@users.noreply.github.com>
2024-04-01 14:36:57 +0900

    [level 3] Title: 베스트앨범, Time: 4.48 ms, Memory: 80.8 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -2,36 +2,45 @@ import java.util.*;
 class Solution {
     public int[] solution(String[] genres, int[] plays) {
         ArrayList<Integer> answer = new ArrayList<>();
+        //(장르, 총 재생 수)
         HashMap<String, Integer> genreMap = new HashMap<>();
+        //(장르, (고유번호, 재생 수))
         HashMap<String, HashMap<Integer, Integer>> musicMap = new HashMap<>();
         
         for(int i =0; i<plays.length; i++){
             if(!genreMap.containsKey(genres[i])){
-                HashMap<Integer, Integer> map = new HashMap<>();
+                //(고유번호, 재생 수)
+                HashMap<Integer, Integer> music = new HashMap<>();
                 genreMap.put(genres[i], plays[i]);
-                map.put(i, plays[i]);
-                musicMap.put(genres[i], map);
+                music.put(i,plays[i]);
+                musicMap.put(genres[i], music);
+                
             }else{
                 genreMap.put(genres[i], genreMap.get(genres[i])+plays[i]);
                 musicMap.get(genres[i]).put(i,plays[i]);
             }
         }
         
-        List<String> keySet = new ArrayList<>(genreMap.keySet());
-        Collections.sort(keySet, (s1,s2) -> genreMap.get(s2) - genreMap.get(s1));
+        //genreMap에 있는 key를 재생 수 기준으로 내림차순 정렬
+        List<String> genre_key = new ArrayList<>(genreMap.keySet());
+        Collections.sort(genre_key, (s1,s2) -> genreMap.get(s2) - genreMap.get(s1));
         
-        for(String s : keySet){
-            HashMap<Integer, Integer> hm = musicMap.get(s);
-            List<Integer> key = new ArrayList<>(hm.keySet());
-            Collections.sort(key, (s1,s2) -> hm.get(s2) - hm.get(s1));
+        //genre_key에서 재생 수 내림차순으로 2개의 노래 선택
+        for(String key : genre_key){
+            //genre_key에 해당하는 (고유번호, 재생 수)
+            HashMap<Integer, Integer> music = new HashMap<>(musicMap.get(key));
+            //노래 고유번호 추출
+            List<Integer> music_num = new ArrayList(music.keySet());
+            //재생 수로 내림차순
+            Collections.sort(music_num, (s1,s2) -> music.get(s2) - music.get(s1));
             
-            answer.add(key.get(0));
-            if(key.size()>1){
-                answer.add(key.get(1));
+            answer.add(music_num.get(0));
+            
+            if(music_num.size() > 1){
+                answer.add(music_num.get(1));
             }
         }
         
-    
         return answer.stream().mapToInt(Integer::intValue).toArray();
     }
 }
