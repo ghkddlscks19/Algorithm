f7e7573cc61dd875c852f635518ac906b67d8b3c
ghkddlscks19 <84233046+ghkddlscks19@users.noreply.github.com>
2024-04-10 01:19:48 +0900

    [level 3] Title: 길 찾기 게임, Time: 2.53 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/3/42892. 길 찾기 게임/길 찾기 게임.java
+++ 프로그래머스/3/42892. 길 찾기 게임/길 찾기 게임.java
@@ -2,12 +2,11 @@ import java.util.*;
 class Solution {
     
     static class Node{
-        int x;
-        int y;
-        int num;
-        Node left;
-        Node right;
-        
+        int num; //번호
+        int x; //x좌표
+        int y; //y좌표
+        Node left; //트리의 왼쪽 가지
+        Node right; //트리의 오른쪽 가지
         public Node(int num, int x, int y){
             this.num = num;
             this.x = x;
@@ -15,71 +14,89 @@ class Solution {
         }
     }
     
-    static Node makeBinaryTree(int[][] nodeinfo){
-        //노드 좌표, 번호 저장할 배열
-        Node[] nodes = new Node[nodeinfo.length];
-        for(int i =0; i<nodeinfo.length; i++){
-            nodes[i] = new Node(i+1, nodeinfo[i][0], nodeinfo[i][1]);
+    //노드 정보를 저장할 리스트 선언
+    static ArrayList<Node> list;
+
+    //전위 순회, 후위 순회 저장할 리스트 선언
+    static ArrayList<Integer> preList = new ArrayList<>();
+    static ArrayList<Integer> postList = new ArrayList<>();
+    
+    static Node makeTree(int[][] nodeinfo){
+        list = new ArrayList<>();
+        
+        int n = 1;
+        for(int[] node : nodeinfo){
+            list.add(new Node(n++, node[0], node[1]));
         }
         
-        Arrays.sort(nodes, (o1, o2) -> {
-            if(o1.y == o2.y) return Integer.compare(o1.x, o2.x);
-            return Integer.compare(o2.y, o1.y);
+        //y가 다르면 y로 내림차순 정렬, y가 같으면 x로 오름차순 정렬
+        Collections.sort(list, (node1, node2) -> {
+           if(node2.y != node1.y){
+               return node2.y - node1.y;
+           }else{
+               return node1.x - node2.x;
+           } 
         });
         
-        Node root = nodes[0];
+        //트리 root 값 설정
+        Node root = list.get(0);
         
-        for(int i =1; i<nodes.length; i++){
+        for(int i = 1; i<list.size(); i++){
             Node parent = root;
+            
             while(true){
-                 if(parent.x > nodes[i].x){
+                //현재 노드의 x값이 부모 노드의 x값보다 작을 때(왼쪽)
+                if(list.get(i).x < parent.x){
+                    //부모 노드의 왼쪽 자식이 null일 때 넣어줌
                     if(parent.left == null){
-                        parent.left = nodes[i];
+                        parent.left = list.get(i);
                         break;
-                    }else{
+                    }else{ //있다면 왼쪽 자식이 다시 부모 노드가 된다.
                         parent = parent.left;
                     }
-                }else{
+                }else{ //현재 노드의 x값이 부모 노드의 x값보다 클 때(오른쪽)
+                    //부모 노드의 오른쪽 자식이 null일 때 넣어줌
                     if(parent.right == null){
-                        parent.right = nodes[i];
+                        parent.right = list.get(i);
                         break;
-                    }else{
+                    }else{ //있다면 오른쪽 자식이 다시 부모 노드가 된다.
                         parent = parent.right;
                     }
-                }   
-            }    
+                }
+            }
         }
-        return nodes[0];
+        return list.get(0);
     }
-    
-    static void preOrder(Node curr, ArrayList<Integer> answer){
+    //전위순회
+    static void preOrder(Node curr){
         if(curr == null){
             return;
+        }else{
+            preList.add(curr.num);
+            preOrder(curr.left);
+            preOrder(curr.right);
         }
-        answer.add(curr.num);
-        preOrder(curr.left, answer);
-        preOrder(curr.right, answer);
     }
     
-    static void postOrder(Node curr, ArrayList<Integer> answer){
+    //후위순회
+    static void postOrder(Node curr){
         if(curr == null){
             return;
+        }else{
+            postOrder(curr.left);
+            postOrder(curr.right);
+            postList.add(curr.num);
         }
-        postOrder(curr.left, answer);
-        postOrder(curr.right, answer);
-        answer.add(curr.num);
     }
+    
     
     public int[][] solution(int[][] nodeinfo) {
-        Node root = makeBinaryTree(nodeinfo);
-        ArrayList<Integer> preOrderList = new ArrayList<>();
-        preOrder(root, preOrderList);
-        ArrayList<Integer> postOrderList = new ArrayList<>();
-        postOrder(root, postOrderList);
-        
-        int answer[][] = new int[2][nodeinfo.length];
-        answer[0] = preOrderList.stream().mapToInt(Integer::intValue).toArray();
-        answer[1] = postOrderList.stream().mapToInt(Integer::intValue).toArray();
+        Node root = makeTree(nodeinfo); 
+        int[][] answer = new int[2][nodeinfo.length];
+        preOrder(root);
+        postOrder(root);
+        answer[0] = preList.stream().mapToInt(Integer::intValue).toArray();
+        answer[1] = postList.stream().mapToInt(Integer::intValue).toArray();
         
         return answer;
     }
