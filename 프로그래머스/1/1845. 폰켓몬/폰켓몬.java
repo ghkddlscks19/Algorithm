66e46ff92089b931fece8215ab0f12a1b056cfd2
ghkddlscks19 <84233046+ghkddlscks19@users.noreply.github.com>
2024-04-12 14:34:01 +0900

    [level 1] Title: 폰켓몬, Time: 6.03 ms, Memory: 77 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] nums) {
+        int answer = 0;
+        //가지고 갈 수 있는 폰켓몬 수
+        int takeCnt = nums.length / 2;
+        //중복되는 건 제거해서 넣을 해시셋
+        HashSet<Integer> mon = new HashSet<>();
+        
+        for(int num : nums){
+            mon.add(num);
+        }
+            
+        if(mon.size() >= takeCnt){
+            answer = takeCnt;
+        }else{
+            answer = mon.size();
+        }
+        return answer;
+    }
+}
