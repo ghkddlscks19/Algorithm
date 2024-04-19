268f6b9cd7405422f542d558da30cba9821afe03
ghkddlscks19 <84233046+ghkddlscks19@users.noreply.github.com>
2024-04-19 16:57:36 +0900

    [level 2] Title: 배달, Time: 1.54 ms, Memory: 77.3 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.java
+++ 프로그래머스/2/12978. 배달/배달.java
@@ -1,50 +1,46 @@
 import java.util.*;
 class Solution {
     static ArrayList<Node> map[];
-    static int[] dist;
-    static boolean[] visited;
+    static int dist[];
     static class Node{
         int v;
-        int dis;
+        int cost;
         
-        public Node(int v, int dis){
+        public Node(int v, int cost){
             this.v = v;
-            this.dis = dis;
+            this.cost = cost;
         }
     }
-    
     public int solution(int N, int[][] road, int K) {
         int answer = 0;
         map = new ArrayList[N+1];
+        
         for(int i =0; i<N+1; i++){
             map[i] = new ArrayList<>();
         }
         
-        for(int[] node : road){
-            map[node[0]].add(new Node(node[1], node[2]));
-            map[node[1]].add(new Node(node[0], node[2]));
+        for(int info[] : road){
+            map[info[0]].add(new Node(info[1], info[2]));
+            map[info[1]].add(new Node(info[0], info[2]));
         }
         
         dist = new int[N+1];
         Arrays.fill(dist, Integer.MAX_VALUE);
-        visited = new boolean[N+1];
+        int start = 1;
+        dist[start] = 0;
         
-        PriorityQueue<Node> pq = new PriorityQueue<>((o1,o2) ->{
-            return o1.dis - o2.dis;
+        PriorityQueue<Node> pq = new PriorityQueue<>((n1, n2) -> {
+            return n1.cost - n2.cost;
         });
         
-        dist[1] = 0;
         pq.add(new Node(1,0));
         
         while(!pq.isEmpty()){
             Node now = pq.poll();
             
-            if(visited[now.v]) continue;
-            
-            visited[now.v] = true;
             for(Node next : map[now.v]){
-                if(dist[next.v] > now.dis + next.dis){
-                    dist[next.v] = now.dis + next.dis;
+                if(dist[next.v] > now.cost + next.cost){
+                    dist[next.v] = now.cost + next.cost; 
                     pq.add(new Node(next.v, dist[next.v]));
                 }
             }
@@ -53,7 +49,7 @@ class Solution {
         for(int x : dist){
             if(x<=K) answer++;
         }
-
+        
         return answer;
     }
 }
