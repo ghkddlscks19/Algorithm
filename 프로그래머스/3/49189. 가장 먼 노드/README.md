9b8e78f0f8dfbbe1455b82730744db4f79b16f92
ghkddlscks19 <84233046+ghkddlscks19@users.noreply.github.com>
2024-04-24 00:49:14 +0900

    [level 3] Title: 가장 먼 노드, Time: 33.02 ms, Memory: 111 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/README.md
+++ 프로그래머스/3/49189. 가장 먼 노드/README.md
@@ -1,10 +1,10 @@
 # [level 3] 가장 먼 노드 - 49189 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49189#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49189) 
 
 ### 성능 요약
 
-메모리: 109 MB, 시간: 43.74 ms
+메모리: 111 MB, 시간: 33.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 20일 16:54:57
+2024년 04월 24일 00:49:13
 
 ### 문제 설명
 
--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -1,16 +1,19 @@
 import java.util.*;
 class Solution {
+    
+    //연결관계를 넣을 인접리스트
     static ArrayList<Integer> graph[];
+    //방문 여부 및 각 노드의 최단경로 거리를 저장할 배열
     static int dist[];
     
-    public void bfs(int x){
+    static void bfs(int n){
+        //bfs를 위한 큐 생성
         ArrayDeque<Integer> queue = new ArrayDeque<>();
-        queue.add(x);
-        dist[x] = 1;
+        dist[n] = 1;
+        queue.add(n);
         
         while(!queue.isEmpty()){
             int now = queue.poll();
-            
             for(int next : graph[now]){
                 if(dist[next] == 0){
                     dist[next] = dist[now]+1;
@@ -18,34 +21,38 @@ class Solution {
                 }
             }
         }
+        
     }
     public int solution(int n, int[][] edge) {
         int answer = 0;
+        
         graph = new ArrayList[n+1];
         for(int i =0; i<n+1; i++){
             graph[i] = new ArrayList<>();
         }
         
+        //양방향 연결관계
         for(int e[] : edge){
             graph[e[0]].add(e[1]);
             graph[e[1]].add(e[0]);
         }
         
         dist = new int[n+1];
- 
+        
+        //bfs를 통해 각 노드의 최단경로 거리 넣기
         bfs(1);
+        
+        //가장 멀리 떨어진 노드의 거리
         int max = 0;
-        //배열에서 가장 멀리 떨어진 거리 구하기
-        for(int i =1; i<n+1; i++){
+        for(int i =0; i<dist.length; i++){
             max = Math.max(max, dist[i]);
         }
         
-        //가장 멀리 떨어진 거리랑 같으면 answer 증가
-        for(int i =1; i<n+1; i++){
+        //가장 멀리 떨어진 노드의 거리 개수 세기
+        for(int i =0; i<dist.length; i++){
             if(max == dist[i]) answer++;
         }
+        
         return answer;
-        
-        
     }
 }
