3011a683a891479f06f96bb12af31f0ea6725075
ghkddlscks19 <84233046+ghkddlscks19@users.noreply.github.com>
2024-05-18 14:58:41 +0900

    [level 2] Title: 전화번호 목록, Time: 286.86 ms, Memory: 96.7 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,19 +1,31 @@
 import java.util.*;
 class Solution {
+//     public boolean solution(String[] phone_book) {
+//         boolean answer = true;
+//         HashMap<String, Integer> phoneMap = new HashMap<>();
+        
+//         for(String phone : phone_book){
+//             phoneMap.put(phone, phoneMap.getOrDefault(phone, 0) + 1);
+//         }
+        
+//         for(String s : phoneMap.keySet()){
+//             for(int i =1; i<s.length(); i++){
+//                 String str = s.substring(0,i);
+//                 if(phoneMap.containsKey(str)) return false;
+//             }
+            
+//         }
+//         return answer;
+//     }
+    
     public boolean solution(String[] phone_book) {
         boolean answer = true;
-        HashMap<String, Integer> phoneMap = new HashMap<>();
         
-        for(String phone : phone_book){
-            phoneMap.put(phone, phoneMap.getOrDefault(phone, 0) + 1);
-        }
-        
-        for(String s : phoneMap.keySet()){
-            for(int i =1; i<s.length(); i++){
-                String str = s.substring(0,i);
-                if(phoneMap.containsKey(str)) return false;
+        Arrays.sort(phone_book);
+        for(int i =0; i<phone_book.length -1; i++){
+            if(phone_book[i+1].startsWith(phone_book[i])){
+                return false;
             }
-            
         }
         return answer;
     }
