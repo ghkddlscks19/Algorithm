40001457cfa2a7268da9d998b2662e8bbcd3c4c8
ghkddlscks19 <84233046+ghkddlscks19@users.noreply.github.com>
2024-12-04 23:20:24 +0900

    [Bronze V] Title: 꼬마 정민, Time: 104 ms, Memory: 14180 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+        StringTokenizer st = new StringTokenizer(str);
+        
+        long a = Long.parseLong(st.nextToken());
+        long b = Long.parseLong(st.nextToken());
+        long c = Long.parseLong(st.nextToken());
+        long sum = a + b + c;
+
+        System.out.println(sum);
+
+    }
+}
