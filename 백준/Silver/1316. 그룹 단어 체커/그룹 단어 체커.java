281e22ce1fa8e9b1ce062cd4beb106e713ae2157
ghkddlscks19 <84233046+ghkddlscks19@users.noreply.github.com>
2024-12-10 10:34:30 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 104 ms, Memory: 14420 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
@@ -1,41 +1,39 @@
-import java.util.Scanner;
-
+import java.util.*;
+import java.io.*;
 public class Main {
-	static Scanner sc = new Scanner(System.in);
-	public static void main(String[] args) {
-		int count = 0;
-		int n = sc.nextInt();
-		for(int i =0; i<n; i++) {
-			if(Ischeck() == true) {
-				count++;
-			}
-		}
-		System.out.println(count);
-	}
-	public static boolean Ischeck() {
-		boolean check[] = new boolean[26];
-		int pr = 0;
-		String s = sc.next();
-		
-		for(int i=0; i<s.length();i++) {
-			int now = s.charAt(i);
-			
-			if(pr!=now) {
-				
-				if(check[now-'a'] == false) {
-					check[now-'a'] = true;
-					pr = now;
-				}else {
-					return false;
-				}
-			}
-			else {
-				continue;
-			}
-		}
-			
-		return true;
-	}
-	
-	
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int cnt = 0;
+
+        for(int i =0; i<N; i++) {
+            String input = br.readLine();
+            if (isValid(input)){
+                cnt++;
+            }
+        }
+
+        System.out.println(cnt);
+    }
+
+    static boolean isValid(String input) {
+        boolean alphabet[] = new boolean[26];
+        int record = 0;
+
+        for(int i = 0; i<input.length(); i++) {
+            int now = input.charAt(i);
+
+            if(record != now) {
+                if(!alphabet[now - 'a']) {
+                    alphabet[now - 'a'] = true;
+                    record = now;
+                }else {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
 }
