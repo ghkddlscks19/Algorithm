e9766c1c253c9e1ce93f4d41c2cfcc02eb4168cf
ghkddlscks19 <84233046+ghkddlscks19@users.noreply.github.com>
2025-01-28 14:55:01 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 124 ms, Memory: 14660 KB -BaekjoonHub

---
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -0,0 +1,45 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+
+  public static void main(String args[]) throws IOException{
+
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    int arr[] = new int[9];
+    int sum = 0;
+    for(int i =0; i<9; i++) {
+      arr[i] = Integer.parseInt(br.readLine());
+      sum += arr[i];
+    }
+
+    Arrays.sort(arr);
+
+    int a = 0, b = 0;
+
+    for(int i =0; i<arr.length-1; i++) {
+      for(int j =i+1; j<arr.length; j++) {
+        if(sum - arr[i] - arr[j] == 100) {
+          // 일곱 난쟁이가 100인 경우 0으로 초기화 해서 없애줌
+          a = i;
+          b = j;
+          break;
+        }
+      }
+    }
+    
+    for(int i=0;i<arr.length;i++) {     
+        if(i!=a && i!=b) {
+            bw.write(arr[i] + "\n");
+        }
+    }
+
+    bw.flush();
+    bw.close();
+  }
+  
+}
+
+
