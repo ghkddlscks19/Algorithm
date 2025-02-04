311608c4194c73429525415bfe0dd436aebfeb82
ghkddlscks19 <84233046+ghkddlscks19@users.noreply.github.com>
2025-02-04 14:08:29 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 100 ms, Memory: 14244 KB -BaekjoonHub

--- 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.java
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.java
@@ -1,20 +1,27 @@
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
 
-public class Main {
+public class Main{
+  public static void main(String args[]) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int A = sc.nextInt();
-		int B = sc.nextInt();
-		int V = sc.nextInt();
-		
-		int day = (V-B)/(A-B);
-		
-		if((V-B)%(A-B)!=0) {
-			day++;
-		}
-		System.out.println(day);		
-	
+    StringTokenizer st = new StringTokenizer(br.readLine());
+
+    int A = Integer.parseInt(st.nextToken());
+    int B = Integer.parseInt(st.nextToken());
+    int V = Integer.parseInt(st.nextToken());
+
+    // 마지막에 B만큼 내려갈 일이 없으니까 최종거리에서 -B한 거리에 하루에 갈 수 있는 거리 나눠주기
+    int day = (V-B) / (A-B);
+
+    // 나머지가 0이 아니라면 낮에 도착하지 않은 경우
+    if((V-B) % (A-B) != 0) {
+      day++;
 	}
 
+    bw.write(day + "");
+    bw.flush();
+    bw.close();
+  }
 }
