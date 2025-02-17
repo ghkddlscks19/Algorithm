3930f2bfeaf2235ee8610d55d35810876f4fd8c8
ghkddlscks19 <84233046+ghkddlscks19@users.noreply.github.com>
2025-02-17 23:07:33 +0900

    [Silver IV] Title: 요세푸스 문제 0, Time: 180 ms, Memory: 21148 KB -BaekjoonHub

---
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
@@ -0,0 +1,38 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+  public static void main(String args[]) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    StringTokenizer st = new StringTokenizer(br.readLine());
+
+    int N = Integer.parseInt(st.nextToken());
+    int K = Integer.parseInt(st.nextToken());
+
+    ArrayDeque<Integer> queue = new ArrayDeque<>();
+
+    for(int i =1; i<=N; i++) {
+      queue.offer(i);
+    }
+
+    int cnt = 1;
+    bw.write("<");
+    while(queue.size() > 1) {
+      int poll = queue.poll();
+      if(cnt == K) {
+        bw.write(poll + ", ");
+        cnt = 1;
+      } else {
+        cnt++;
+        queue.offer(poll);
+      }
+    }
+
+    bw.write(queue.poll() + ">");
+
+    bw.flush();
+    bw.close();
+  }
+}
