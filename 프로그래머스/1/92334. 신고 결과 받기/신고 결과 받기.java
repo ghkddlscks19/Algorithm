c7e092e4dc01905e9a9554effd470def200e1350
ghkddlscks19 <84233046+ghkddlscks19@users.noreply.github.com>
2025-03-05 09:16:11 +0900

    [level 1] Title: 신고 결과 받기, Time: 169.57 ms, Memory: 166 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -1,40 +1,46 @@
 import java.util.*;
 class Solution {
     public int[] solution(String[] id_list, String[] report, int k) {
+        // 신고 해시맵 (신고 당한 사람, 신고한 사람(중복 X))
+        HashMap<String, HashSet<String>> reportMap = new HashMap<>();
+        
+        // 각 유저 별 신고 당한 횟수
+        HashMap<String, Integer> countMap = new HashMap<>();
+        
+        // 신고 해시맵에 넣어 주기
+        for(String rep: report) {
+            String tmp[] = rep.split(" ");
+            String reportUser = tmp[0]; // 신고한 사람
+            String reportedUser = tmp[1]; // 신고 당한 사람
+            
+            // 현재 해시맵에 들어있지 않다면 초기화 해주기
+            if(!reportMap.containsKey(reportedUser)) {
+                reportMap.put(reportedUser, new HashSet<>());
+            }
+             
+            reportMap.get(reportedUser).add(reportUser);
+            
+        }
+        
+        for(String reportedUser: reportMap.keySet()) {
+            // 신고를 k건 이상 받았을 때 정지 사실 메일로 발송하기
+            if(reportMap.get(reportedUser).size() >= k) {
+                
+                List<String> list = new ArrayList<>(reportMap.get(reportedUser));
+                
+                for(String str: list) {
+                    countMap.put(str, countMap.getOrDefault(str, 0) + 1);
+                }
+            }
+        }
+        
+        
         int[] answer = new int[id_list.length];
-        //(신고 당한 사람, 신고한 사람<중복x>)
-        HashMap<String, HashSet<String>> reported = new HashMap<>();
-        //(신고한 사람, 받을 메일 횟수)
-        HashMap<String, Integer> count = new HashMap<>();
         
-        for(String s : report){
-            HashSet<String> hs = new HashSet<>();
-            String str[] = s.split(" ");
-            String reportUser = str[0];
-            String reportedUser = str[1];
-            
-            if(!reported.containsKey(reportedUser)){
-                reported.put(reportedUser, new HashSet<>());
-            }
-            reported.get(reportedUser).add(reportUser);
-            
+        for(int i =0; i<id_list.length; i++) {
+            answer[i] = countMap.getOrDefault(id_list[i], 0);
         }
         
-        for(String s : reported.keySet()){
-            if(reported.get(s).size() >= k){
-                List<String> list = new ArrayList<>(reported.get(s));
-                for(String str : list){
-                    count.put(str, count.getOrDefault(str, 0) + 1);
-                } 
-                    
-            }
-        }
-        
-        for(int i =0; i<id_list.length; i++){
-            answer[i] = count.getOrDefault(id_list[i], 0);
-        }
-        
-     
         return answer;
     }
 }
