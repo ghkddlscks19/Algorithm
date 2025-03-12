0ce95132915d4d1b7b74fd75559b7758347d8e9f
ghkddlscks19 <84233046+ghkddlscks19@users.noreply.github.com>
2025-03-12 10:06:19 +0900

    [Silver III] Title: 1로 만들기, Time: 124 ms, Memory: 18384 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,26 +1,30 @@
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
 
-public class Main {
+public class Main{
+  public static void main(String args[]) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-    public static void main(String[] args) {
-        Main T = new Main();
-        Scanner sc = new Scanner(System.in);
+    // 정수 입력 받기
+    int X = Integer.parseInt(br.readLine());
 
-        int n = sc.nextInt();
-        int dp[] = new int[n + 1];
+    int dp[] = new int[1000001];
 
-        dp[0] = dp[1] = 0;
-
-        for (int i = 2; i <= n; i++) {
-            dp[i] = dp[i - 1] + 1;
-            if (i % 2 == 0) {
-                dp[i] = Math.min(dp[i], dp[i / 2] + 1);
-            }
-            if (i % 3 == 0) {
-                dp[i] = Math.min(dp[i], dp[i / 3] + 1);
-            }
-        }
-
-        System.out.println(dp[n]);
+    dp[0] = dp[1] = 0;
+    
+    for(int i =2; i<=X; i++) {
+      dp[i] = dp[i-1] + 1;
+      if(i % 3 == 0) {
+        dp[i] = Math.min(dp[i], dp[i/3] + 1);
+      }
+      if(i % 2 == 0) {
+        dp[i] = Math.min(dp[i], dp[i/2] + 1);   
+      }
     }
+
+    bw.write(dp[X] + "");
+    bw.flush();
+    bw.close();
+  }
 }
