f8ca457b18f5de9f60bed54d0f5c738318ba652b
ghkddlscks19 <84233046+ghkddlscks19@users.noreply.github.com>
2025-03-20 10:08:36 +0900

    [Silver III] Title: 계단 오르기, Time: 104 ms, Memory: 14244 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -1,29 +1,51 @@
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
 
-public class Main {
+public class Main{
+  public static void main(String args[]) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-    public static void main(String[] args) {
-        Main T = new Main();
-        Scanner sc = new Scanner(System.in);
+    int N = Integer.parseInt(br.readLine());
 
-        int n = sc.nextInt();
-        int arr[] = new int[n+1];
-        int dp[] = new int[n+1];
+    int stair[] = new int[N+1];
+    int dp[] = new int[N+1];
 
-        for (int i = 1; i <= n; i++) {
-            arr[i] = sc.nextInt();
-        }
-
-        dp[1] = arr[1];
-        if (n > 1) {
-            dp[2] = arr[1] + arr[2];
-
-        }
-        for (int i = 3; i <= n; i++) {
-            dp[i] = Math.max(dp[i - 3] + arr[i - 1], dp[i-2]) + arr[i];
-        }
-
-        System.out.println(dp[n]);
-
+    for(int i =1; i<=N; i++) {
+      stair[i] = Integer.parseInt(br.readLine());
     }
+
+    dp[1] = stair[1];
+    if(N == 1) {
+      bw.write(dp[1] + "");
+      bw.flush();
+      bw.close();
+      return;
+    }
+    dp[2] = stair[1] + stair[2];
+    if(N == 2) {
+      bw.write(dp[2] + "");
+      bw.flush();
+      bw.close();
+      return;      
+    }
+    dp[3] = Math.max(stair[1], stair[2]) + stair[3];
+    if(N == 3) {
+      bw.write(dp[3] + "");
+      bw.flush();
+      bw.close();
+      return;      
+    }
+    
+    for(int i =4; i<=N; i++) {
+      dp[i] = Math.max(dp[i-2], dp[i-3] + stair[i-1]) + stair[i];
+    }
+
+    bw.write(dp[N] + "");
+    bw.flush();
+    bw.close();
+
+    
+    
+  }
 }
