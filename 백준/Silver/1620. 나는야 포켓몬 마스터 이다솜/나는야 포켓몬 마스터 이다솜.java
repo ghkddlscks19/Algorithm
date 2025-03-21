f40e14dd682c320686d1ccdce372dbbc1f4259b2
ghkddlscks19 <84233046+ghkddlscks19@users.noreply.github.com>
2025-03-21 09:37:47 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 460 ms, Memory: 49956 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
@@ -9,10 +9,12 @@ public class Main{
     StringTokenizer st = new StringTokenizer(br.readLine());
     int N = Integer.parseInt(st.nextToken());
     int M = Integer.parseInt(st.nextToken());
-    
+
+    // 숫자로 찾아야 할 때도 있고 문자로 찾아야 할 때도 있으니까 맵 2개 만들기
     HashMap<String, Integer> hm1 = new HashMap<>();
     HashMap<Integer, String> hm2 = new HashMap<>();
-    
+
+    // 해시맵에 넣어주기 
     for(int i =1; i<=N; i++) {
       String pkm = br.readLine();
       hm1.put(pkm, i);
@@ -21,12 +23,14 @@ public class Main{
 
     for(int i =0; i<M; i++) {
       String input = br.readLine();
-      if(input.charAt(0) >= '1' && input.charAt(0) <= '9') {
-        int num = Integer.parseInt(input);
-        bw.write(hm2.get(num) + "\n");
+      // 포켓몬 이름으로 들어왔을 때
+      if (input.charAt(0) >= 'A' && input.charAt(0) <= 'Z') {
+        bw.write(hm1.get(input) + "");
       } else {
-        bw.write(hm1.get(input) + "\n");
+        bw.write(hm2.get(Integer.parseInt(input)));
       }
+
+      bw.newLine();
     }
 
     bw.flush();
