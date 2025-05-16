5e812e158e4528b1057480c887ab8010ddaa6e2e
ghkddlscks19 <84233046+ghkddlscks19@users.noreply.github.com>
2025-05-16 13:52:52 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 100 ms, Memory: 14092 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -1,30 +1,26 @@
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
+public class Main{
+  public static void main(String args[]) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-public class Main {
+    int T = Integer.parseInt(br.readLine());
 
-    static int t;
-    static int arr[];
+    int arr[] = new int[11];
+    arr[1] = 1; arr[2] = 2; arr[3] = 4;
 
-
-    public static void main(String[] args) {
-        Main T = new Main();
-        Scanner sc = new Scanner(System.in);
-
-        int t = sc.nextInt();
-
-        int dp[] = new int[11];
-
-        dp[1] = 1;
-        dp[2] = 2;
-        dp[3] = 4;
-
-        for (int i = 4; i <= 10; i++) {
-            dp[i] = dp[i - 3] + dp[i - 2] + dp[i - 1];
-        }
-
-        for (int i = 0; i < t; i++) {
-            int n = sc.nextInt();
-            System.out.println(dp[n]);
-        }
+    for(int i =4; i<11; i++){
+      arr[i] = arr[i - 1] + arr[i - 2] + arr[i - 3];  
     }
+    
+    for(int i =0; i<T; i++) {
+      int num = Integer.parseInt(br.readLine());
+      bw.write(arr[num] + "");
+      bw.newLine();
+    }
+
+    bw.flush();
+    bw.close();
+  }
 }
