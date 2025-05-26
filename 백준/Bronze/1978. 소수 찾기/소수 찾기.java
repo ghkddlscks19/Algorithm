52c89a93c2049eac1b87afa5beb03f7861c6bfeb
ghkddlscks19 <84233046+ghkddlscks19@users.noreply.github.com>
2025-05-27 08:09:03 +0900

    [Bronze II] Title: 소수 찾기, Time: 100 ms, Memory: 14176 KB -BaekjoonHub

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.java
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.java
@@ -1,35 +1,30 @@
 import java.util.*;
 import java.io.*;
-
 public class Main{
   public static void main(String args[]) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-    // 1000까지 소수인 수 체크
-    int arr[] = new int[1001];
-    arr[1] = 1;
-
-    for(int i = 2; i<arr.length; i++) {
-      for(int j = i*i; j<arr.length; j+=i) {
-        arr[j] = 1;
-      }
-    }
-    
     int N = Integer.parseInt(br.readLine());
-    StringTokenizer st = new StringTokenizer(br.readLine());
-    int answer = 0;
 
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    int cnt = 0;
     for(int i =0; i<N; i++) {
       int num = Integer.parseInt(st.nextToken());
-      if(arr[num] == 0) answer++;
+      if(isPrime(num)) cnt++;
     }
 
-    bw.write(answer + "");
+    bw.write(String.valueOf(cnt));
     bw.flush();
     bw.close();
+    
+  }
 
+  static boolean isPrime(int n) {
+    if(n == 1) return false;
+    for(int i = 2; i*i<=n; i++) {
+      if(n % i == 0) return false;
+    }
+    return true;
   }
 }
-
-
