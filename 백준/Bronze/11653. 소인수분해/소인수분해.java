0769a14fbcb6936ac5d118e9f4f51a9a4505031f
ghkddlscks19 <84233046+ghkddlscks19@users.noreply.github.com>
2025-05-27 08:25:37 +0900

    [Bronze I] Title: 소인수분해, Time: 128 ms, Memory: 14372 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.java
+++ 백준/Bronze/11653. 소인수분해/소인수분해.java
@@ -1,23 +1,25 @@
 import java.util.*;
 import java.io.*;
-
 public class Main{
-  public static void main(String args[]) throws IOException{
+  public static void main(String args[]) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
     int N = Integer.parseInt(br.readLine());
 
-    int i = 2;
-    while(N > 1) {
-      if(N % i == 0) {
+    for(int i =2; i*i<=N; i++) {
+      while(N % i == 0) {
+        bw.write(String.valueOf(i));
+        bw.newLine();
         N /= i;
-        bw.write(i + "\n");
-      } else {
-        i++;
       }
     }
 
+    if(N != 1) {
+      bw.write(String.valueOf(N));
+      bw.newLine();
+    }
+    
     bw.flush();
     bw.close();
   }
