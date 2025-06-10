f16eb20506da3a2b961f2e677f507cb95a1359b3
ghkddlscks19 <84233046+ghkddlscks19@users.noreply.github.com>
2025-06-11 00:37:47 +0900

    [Bronze V] Title: A+B - 2, Time: 104 ms, Memory: 14200 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+        int sum = a + b;
+        
+        bw.write(String.valueOf(sum));
+        bw.flush();
+        bw.close();
+    }
+}
