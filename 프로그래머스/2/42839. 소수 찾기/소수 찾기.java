ae116b42ff25e86d2f1baa7ff4daa52a289b49a0
ghkddlscks19 <84233046+ghkddlscks19@users.noreply.github.com>
2025-10-13 18:47:01 +0900

    [level 2] Title: 소수 찾기, Time: 73.66 ms, Memory: 99.7 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,54 +1,50 @@
 import java.util.*;
 class Solution {
-    
-    static boolean visited[];
-    static HashSet<Integer> hs;
+    static HashSet<Integer> hs = new HashSet<>();
     static int answer = 0;
-    //각 숫자들을 하나씩 뽑아 모든 숫자 경우의 수 만들기
-    static void dfs(String numbers, String curr, int depth){
+    static boolean[] visited;
+    public int solution(String numbers) {
+        visited = new boolean[numbers.length()];
+        dfs(numbers, "", 0);
+        isPrime();
+        return answer;
+    }
+    
+    // 종이 조각 전부 만들기
+    static void dfs(String numbers, String curr, int depth) {
         if(depth == numbers.length()) return;
         
-        for(int i =0; i<numbers.length(); i++){
-            if(!visited[i]){
+        for(int i =0; i<numbers.length(); i++) {
+            if(!visited[i]) {
                 visited[i] = true;
                 hs.add(Integer.parseInt(curr + numbers.charAt(i)));
-                dfs(numbers, curr + numbers.charAt(i), depth+1);
+                dfs(numbers, curr + numbers.charAt(i), depth + 1);
                 visited[i] = false;
             }
         }
     }
     
-    static void isPrime(HashSet<Integer> hs){
-        //set을 arrayList로 변환
-        ArrayList<Integer> numList = new ArrayList<>(hs);
-        //최대값 추출
-        int max = Collections.max(numList);
-        System.out.println(max);
-        boolean prime[] = new boolean[max+1];
+    // 소수 판별 후 개수 세기
+    static void isPrime() {
+        int max = 0;
+        for(int x: hs) {
+            max = Math.max(max, x);
+        }
+        
+        boolean[] prime = new boolean[max + 1];
         Arrays.fill(prime, true);
         prime[0] = prime[1] = false;
         
-        //최대값까지 소수 판별해놓기
-        for(int i =2; i<=Math.sqrt(max); i++){
-            if(prime[i]){
-                for(int j = i*i; j<=max; j+=i) {
-			       prime[j] = false;
-        	    }
-            } 
+        for(int i =2; i<=Math.sqrt(max); i++) {
+            if(prime[i]) {
+                for(int j =i*i; j<=max; j+=i) {
+                    prime[j] = false;
+                }              
+            }
         }
         
-        for(int n : numList){
-            //소수일 때 정답 증가
-            if(prime[n] == true) answer++;
+        for(int x: hs) {
+            if(prime[x] == true) answer++;
         }
-    }
-    
-    public int solution(String numbers) {
-        visited = new boolean[numbers.length()];
-        hs = new HashSet<>();
-        dfs(numbers, "", 0);
-        System.out.println(hs);
-        isPrime(hs);
-        return answer;
     }
 }
