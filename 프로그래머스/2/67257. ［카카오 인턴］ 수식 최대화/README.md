692d70d93926c6c6d87eff4c712e713330a4ebc8
ghkddlscks19 <84233046+ghkddlscks19@users.noreply.github.com>
2025-10-15 13:59:56 +0900

    [level 2] Title: [카카오 인턴] 수식 최대화, Time: 4.02 ms, Memory: 93.1 MB -BaekjoonHub

--- 프로그래머스/2/67257. ［카카오 인턴］ 수식 최대화/README.md
+++ 프로그래머스/2/67257. ［카카오 인턴］ 수식 최대화/README.md
@@ -1,10 +1,10 @@
 # [level 2] [카카오 인턴] 수식 최대화 - 67257 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/67257?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/67257) 
 
 ### 성능 요약
 
-메모리: 83.3 MB, 시간: 1.23 ms
+메모리: 93.1 MB, 시간: 4.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 07월 03일 18:35:31
+2025년 10월 15일 13:59:55
 
 ### 문제 설명
 
--- 프로그래머스/2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.java
+++ 프로그래머스/2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.java
@@ -1,55 +1,57 @@
 import java.util.*;
 class Solution {
     public long solution(String expression) {
-        // 연산자 우선순위 경우의 수
-        char priorities[][] = {
+        char[][] priorities = {
             {'+', '-', '*'},
             {'+', '*', '-'},
             {'-', '+', '*'},
             {'-', '*', '+'},
-            {'*', '+', '-'},
             {'*', '-', '+'},
+            {'*', '+', '-'},
         };
         
-        long max = 0;
-        for(char priority[] : priorities) {
-            long result = Math.abs(calc(expression, priority));
-            max = Math.max(max, result);
+        long answer = 0;
+        for(char[] priority: priorities) {
+            answer = Math.max(answer, Math.abs(calc(priority, expression)));
         }
         
-        return max;
+        return answer;
     }
     
-    static long calc(String expression, char priority[]) {
+    static long calc(char[] priority, String expression) {
+        // 숫자와 문자 분리해서 저장
         List<Long> numList = new ArrayList<>();
         List<Character> opList = new ArrayList<>();
         
-        StringBuilder sb = new StringBuilder();
-        for(char c : expression.toCharArray()) {
-            if(c == '*' || c == '+' || c == '-') {
-                numList.add(Long.parseLong(sb.toString()));
-                opList.add(c);
-                sb = new StringBuilder();
-            } else {
-                sb.append(c);
+        String[] numArr = expression.split("[+\\-*]"); // 정규식을 이용한 숫자 추출
+        for(String num: numArr) {
+            numList.add(Long.parseLong(num));
+        }
+        
+        // 문자 추출 후 저장
+        for(char c: expression.replaceAll("[0-9]", "").toCharArray()) {
+            opList.add(c);
+        }
+        
+        // 연산자 우선순위에 맞게 꺼내서 연산
+        for(char op: priority) {
+            // 우선순위에 해당하는 연산자가 있을 때까지 반복
+            while(opList.contains(op)) {
+                int idx = opList.indexOf(op);
+                long a = numList.get(idx);
+                long b = numList.get(idx + 1);
+                long result = operation(a, b, op); 
+                
+                // 연산 후 리스트에서 제거하고 현재 인덱스에 결과 값 추가
+                numList.remove(idx);
+                numList.remove(idx);
+                numList.add(idx, result);
+                opList.remove(idx);
             }
         }
         
-        // 마지막 숫자
-        numList.add(Long.parseLong(sb.toString()));
         
-        // 연산자 우선 순위로 계산
-        for(char op : priority) {
-            for(int i =0; i<opList.size(); i++) {
-                if(opList.get(i) == op) {                 
-                    long result = operation(numList.get(i), numList.get(i+1), op);
-                    numList.set(i, result);
-                    opList.remove(i);
-                    numList.remove(i+1);
-                    i--;
-                }
-            }
-        }
+        
         return numList.get(0);
     }
     
