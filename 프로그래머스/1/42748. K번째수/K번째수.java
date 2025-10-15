f5bf695e1c0f5543f83e04700f3e2bed7d0dc8ad
ghkddlscks19 <84233046+ghkddlscks19@users.noreply.github.com>
2025-10-15 12:47:39 +0900

    [level 1] Title: K번째수, Time: 0.34 ms, Memory: 94 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -1,18 +1,17 @@
 import java.util.*;
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
+        int idx = 0;
         int[] answer = new int[commands.length];
-        //커맨드 크기만큼 돌면서 배열을 자르기
-        for(int n =0; n<commands.length; n++){
-            int i = commands[n][0]-1;
-            int j = commands[n][1];
-            int k = commands[n][2]-1;
-            //복사할배열, 시작점, 끝점 -> 인덱스 0을 기준으로 함
-            int copy[] = Arrays.copyOfRange(array, i, j);
-            //나온 배열을 정렬하기
-            Arrays.sort(copy);
-            //k번 째 숫자를 정답 배열에 넣기
-            answer[n] = copy[k];
+        for(int[] comm: commands) {
+            int i = comm[0] - 1; 
+            int j = comm[1];
+            int k = comm[2] - 1; 
+            
+            int[] copy = Arrays.copyOfRange(array, i, j); // 배열 범위만큼 복사
+            Arrays.sort(copy); // 복사한 배열 정렬
+            
+            answer[idx++] = copy[k];
         }
         return answer;
     }
