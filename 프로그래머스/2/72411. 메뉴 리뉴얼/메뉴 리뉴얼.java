8fafad41728584c0368495c8a30af84d9f86705b
ghkddlscks19 <84233046+ghkddlscks19@users.noreply.github.com>
2025-10-21 16:11:59 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 24.15 ms, Memory: 81.8 MB -BaekjoonHub

--- 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
@@ -1,49 +1,47 @@
 import java.util.*;
 class Solution {
-    
-    static HashMap<String, Integer> courseMap = new HashMap<>();
-    
-    public static void combi(int n, char c[], String result){
-        if(result.length() > 1){
-            char sortArray[] = result.toCharArray();
-            Arrays.sort(sortArray);
-            String sortStr = String.valueOf(sortArray);
-            courseMap.put(sortStr, courseMap.getOrDefault(sortStr, 0) + 1);
-        }
-        for(int i =n; i<c.length; i++){
-            combi(i+1, c, result+c[i]);
-        }
-    }
+    static HashMap<String, Integer> hm = new HashMap<>();
     
     public String[] solution(String[] orders, int[] course) {
-        ArrayList<String> answer = new ArrayList<>();
-        
-        for(String s : orders){
-            char[] ordersArray = s.toCharArray();
-            combi(0, ordersArray, "");
+        // orders를 정렬한 상태로 조합 생성하기!
+        for(String order: orders) {
+            char[] orderArray = order.toCharArray();
+            Arrays.sort(orderArray);
+            combi(orderArray, "", 0);
         }
         
-        for(int x : course){
+        List<String> answer = new ArrayList<>();
+        // 길이가 n이고 가장 많이 나온 걸 정답에 저장
+        for(int n: course) {
             int max = 0;
-            for(String s : courseMap.keySet()){
-                if(s.length() == x && courseMap.get(s) >= 2){
-                    max = Math.max(max, courseMap.get(s));
+            // 1. 가장 많이 나오는 개수부터 구하기
+            for(String menu: hm.keySet()) {
+                if(menu.length() == n && hm.get(menu) >= 2) {
+                    max = Math.max(max, hm.get(menu));
                 }
             }
-            for(String s : courseMap.keySet()){
-                if(s.length() == x && courseMap.get(s) == max){
-                    answer.add(s);
+            
+            // 2. 최대랑 같은 메뉴만 추출해서 저장
+            for(String menu: hm.keySet()) {
+                if(menu.length() == n && hm.get(menu) == max) {
+                    answer.add(menu);
                 }
             }
         }
         
         Collections.sort(answer);
         
-    
-        
         return answer.toArray(new String[answer.size()]);
     }
     
-    
-    
+    static void combi(char[] orderArray, String curr, int idx) {
+        // 길이가 2 이상이면 저장하지만 계속 진행
+        if(curr.length() >= 2) {
+            hm.put(curr, hm.getOrDefault(curr, 0) + 1);
+        }
+        
+        for(int i =idx; i<orderArray.length; i++) {
+            combi(orderArray, curr + orderArray[i], i + 1);
+        }
+    }
 }
