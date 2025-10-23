ab7920fb19b7c43c8c0fdd4e71519fc1c4b17272
ghkddlscks19 <84233046+ghkddlscks19@users.noreply.github.com>
2025-10-23 22:23:27 +0900

    [level 3] Title: 입국심사, Time: 47.50 ms, Memory: 95.1 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -1,24 +1,32 @@
 class Solution {
     public long solution(int n, int[] times) {
-        long answer = 0;
+        long start = 1;
+        long end = (long) times[times.length - 1] * (long) n; // 최대 시간 * n이 최대값
         
-        long start = (long)times[0];
-        long end = times[times.length-1] * (long)n;    
+        long answer = end;
         
-        while(start <= end){
+        while(start <= end) {
             long mid = (start + end) / 2;
-            long sum = 0;
-            for(int time : times){
-                sum += mid/time;
+            
+            // mid 시간 동안 n명이 할 수 있는지 확인
+            long cnt = 0;
+            for(int time: times) {
+                cnt += mid / time;
+                
+                if(cnt >= n) break;
             }
             
-            if(sum >= n){
-                end = mid - 1;
+            
+            if(cnt >= n) {
+                // n명 이상 처리가 가능하므로 end 줄이기
                 answer = mid;
-            }else{
+                end = mid - 1;
+            } else {
+                // n명 처리가 안되면 start를 늘리기
                 start = mid + 1;
             }
         }
+        
         return answer;
     }
 }
