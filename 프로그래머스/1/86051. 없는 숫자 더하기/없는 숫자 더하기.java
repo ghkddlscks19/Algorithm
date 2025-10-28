7c011bbb357ee85817ce642c74264a03ecdd01e4
ghkddlscks19 <84233046+ghkddlscks19@users.noreply.github.com>
2025-10-28 23:06:56 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.03 ms, Memory: 85.2 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -1,14 +1,14 @@
 class Solution {
     public int solution(int[] numbers) {
-        boolean check[] = new boolean[10];
+        boolean[] checked = new boolean[10];
+        
+        for(int n: numbers) {
+            checked[n] = true;
+        } 
         
         int answer = 0;
-        for(int i = 0; i<numbers.length; i++) {
-            check[numbers[i]] = true;
-        }
-        
-        for(int i =0; i<check.length; i++) {
-            if(check[i] == false) answer += i;
+        for(int i =0; i<10; i++) {
+            if(checked[i] == false) answer += i;
         }
         
         return answer;
