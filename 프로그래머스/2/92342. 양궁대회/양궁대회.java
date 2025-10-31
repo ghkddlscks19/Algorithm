8f915ee3a497b268cc759129b9c515aa3134c375
ghkddlscks19 <84233046+ghkddlscks19@users.noreply.github.com>
2025-10-31 22:14:33 +0900

    [level 2] Title: 양궁대회, Time: 0.99 ms, Memory: 88.3 MB -BaekjoonHub

--- 프로그래머스/2/92342. 양궁대회/양궁대회.java
+++ 프로그래머스/2/92342. 양궁대회/양궁대회.java
@@ -1,63 +1,68 @@
+import java.util.*;
 class Solution {
-    //최대 점수
-    static int max = 0;
-    //라이언, 어피치, 정답 배열
-    static int apeach[], ryan[], answer[];
-    
-    //인덱스, 현재 쏜 화살 개수, 총 화살 개수
-    static void dfs(int idx, int shot, int n){
-        //다 쐈을 때 점수 계산
-        if(shot == n){
-            int aScore = 0;
-            int rScore = 0;
-            for(int i =0; i<11; i++){
-                //둘 다 0인 경우는 continue
-                if(apeach[i] == 0 && ryan[i] == 0) continue;
-                //라이언이 더 많이 맞췄을 때
-                if(apeach[i] < ryan[i]) rScore += 10-i;
-                else aScore += 10-i;
-            }
-            
-            //점수 차
-            int gap = rScore - aScore;
-            
-            //라이언 점수가 더 클 때
-            if(gap > 0){
-                //최대 점수보다 점수 차가 클 때
-                if(max < gap){
-                    max = gap;
-                    answer = ryan.clone();
-                }else if(max == gap){ // 같을 때는 가장 낮은 점수를 많이 맞힌 경우 찾기 
-                    for(int i =10; i>=0; i--){
-                        if(answer[i] < ryan[i]){
-                            answer = ryan.clone();
-                        }else if(answer[i] > ryan[i]) return;
-                    }
-                }
-            }
-            return;
-        }
-        for(int i = idx; i<11; i++){
-            if(ryan[i] > apeach[i]) continue;
-            ryan[i]++;
-            dfs(i, shot+1, n);
-            ryan[i]--;
-        }
-    }
+    static int[] ryan;
+    static int[] apeach;
+    static int[] result;
+    static int maxDiff = 0;
     
     public int[] solution(int n, int[] info) {
         apeach = info;
         ryan = new int[11];
-        answer = new int[11];
-        dfs(0,0,n);
+        result = new int[11];
         
-        int cnt = 0;
-        for(int i =0; i<11; i++){
-            if(answer[i] == 0){
-                cnt++;
+        dfs(0, n);
+       
+        if(maxDiff <= 0) return new int[]{-1};
+        return result;
+    }
+    
+    static void dfs(int depth, int arrow) {
+        if(depth == 11) {
+            if(arrow > 0) {
+                ryan[10] += arrow;
             }
+            
+            int ryanScore = 0;
+            int apeachScore = 0;
+            
+            // 점수 구하기
+            for(int i =0; i<11; i++) {
+                if(apeach[i] == ryan[i]) continue;
+                else if(apeach[i] > ryan[i]) apeachScore += 10 - i;
+                else ryanScore += 10 - i;
+            }
+            
+            if(maxDiff < ryanScore - apeachScore) {
+                maxDiff = ryanScore - apeachScore;      
+                result = Arrays.copyOf(ryan, 11);
+            } else if(maxDiff == ryanScore - apeachScore) {
+                // 최대 점수 차이가 같을 때는 낮은 점수를 많이 맞춘 배열로 변경
+                for(int i =10; i>=0; i--) {
+                    if(ryan[i] > result[i]) {
+                        result = Arrays.copyOf(ryan, 11);
+                        break;
+                    } else if(ryan[i] < result[i]) {
+                        break;
+                    }
+                }
+            }
+            
+            if(arrow > 0) {
+                ryan[10] -= arrow;
+            }
+            
+            return;
         }
-        if(cnt == 11) return new int[]{-1};
-        return answer;
+        
+        // 화살이 어피치가 쏜거보다 많이 남아있어야 이길 수 있음
+        if(arrow > apeach[depth]) {
+            ryan[depth] = apeach[depth] + 1;
+            dfs(depth + 1, arrow - ryan[depth]);
+            ryan[depth] = 0;
+        }
+        
+        // 이 칸에는 안쏜다
+        dfs(depth + 1, arrow);
+        
     }
 }
