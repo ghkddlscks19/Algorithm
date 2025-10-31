7518a0de5d6ca2e629b7819a8f700a3c2e3d651a
ghkddlscks19 <84233046+ghkddlscks19@users.noreply.github.com>
2025-10-31 20:56:12 +0900

    [level 3] Title: 정수 삼각형, Time: 9.26 ms, Memory: 64.4 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
@@ -2,28 +2,35 @@ import java.util.*;
 class Solution {
     public int solution(int[][] triangle) {
         int len = triangle.length;
+        
         //각 경로에서의 최대 값 저장
         int dp[][] = new int[len][len];
     
         dp[0][0] = triangle[0][0];
         
-        //왼쪽으로만 쭉 내려갔을 때
-        for(int i =1; i<len; i++){
+        // 왼쪽 대각선 합 구하기
+        for(int i =1; i<len; i++) {
             dp[i][0] = dp[i-1][0] + triangle[i][0];
         }
         
-        //나머지 경우
-        for(int i =1; i<len; i++){
-            for(int j=1; j<triangle[i].length; j++){
+        // 오른쪽 대각선 합 구하기
+        for(int i =1; i<len; i++) {
+            dp[i][i] = dp[i-1][i-1] + triangle[i][i];
+        }
+        
+        // 나머지 합
+        for(int i =2; i<len; i++) {
+            for(int j =1; j<i; j++) {
                 dp[i][j] = Math.max(dp[i-1][j-1], dp[i-1][j]) + triangle[i][j];
             }
         }
         
         int answer = 0;
-        //배열 맨 하단에서 최대값 구하기
-        for(int i =0; i<len; i++){
+        
+        for(int i =0; i<len; i++) {
             answer = Math.max(answer, dp[len-1][i]);
         }
+        
         
         return answer;
     }
