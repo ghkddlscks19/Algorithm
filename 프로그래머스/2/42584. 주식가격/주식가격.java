0c543b41a96c35330e082afd2e50a71c452868c7
ghkddlscks19 <84233046+ghkddlscks19@users.noreply.github.com>
2025-11-05 15:00:49 +0900

    [level 2] Title: 주식가격, Time: 19.77 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,27 +1,25 @@
-import java.util.Stack;
+import java.util.*;
 class Solution {
     public int[] solution(int[] prices) {
-        int n = prices.length;
-        int[] answer = new int[n];
-        Stack<Integer> stack = new Stack<>();
+        int len = prices.length;
+        int[] answer = new int[len];
         
-        //0번째 원소는 비교할게 없으니 일단 push
-        stack.push(0);
-        
-        for(int i =1; i<n; i++){
-            //비어있지 않으면서 가격이 떨어졌을 때
-            while(!stack.isEmpty() && prices[stack.peek()] > prices[i]){
-                int pop = stack.pop();
-                answer[pop] = i-pop;
-            }
-            stack.push(i);
+        // 정답 배열을 현재 인덱스에서 끝까지의 거리로 초기화
+        for(int i =0; i<len; i++) {
+            answer[i] = len - i - 1;
         }
         
-        //스택에 남아있는 것들로 주식가격 계산
-        while(!stack.isEmpty()){
-            int pop = stack.pop();
-            //여기 개념을 몰랐음.
-            answer[pop] = n-pop-1;
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+        stack.push(0); // 0번째는 비교할 게 없음
+        
+        for(int i =1; i<len; i++) {
+            // 비어있지 않고 가격이 떨어질 때
+            while(!stack.isEmpty() && prices[stack.peek()] > prices[i]) {
+                int pop = stack.pop();
+                answer[pop] = i - pop;
+            }
+            
+            stack.push(i);
         }
         
         return answer;
