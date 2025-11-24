35968aefa3b6621a5223fbaea6db86c4fc780e48
ghkddlscks19 <84233046+ghkddlscks19@users.noreply.github.com>
2025-11-24 11:43:21 +0900

    [level 2] Title: 큰 수 만들기, Time: 90.50 ms, Memory: 84.2 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -8,12 +8,19 @@ class Solution {
         for(int i =0; i<number.length(); i++) {
             char curr = number.charAt(i);
             
+            // 스택이 비어있지 않고 현재 값이 스택에 있는 값보다 크고 k보다 카운트가 작으면 pop
             while(!stack.isEmpty() && stack.peek() < curr && cnt < k) {
                 stack.pop();
                 cnt++;
             }
             
-            if(stack.size() < number.length() - k) stack.push(curr);
+            stack.push(curr);
+        }
+        
+        // 역순인 경우라 pop이 k만큼 안된경우
+        while(cnt < k) {
+            stack.pop();
+            cnt++;
         }
         
         for(char c: stack) {
