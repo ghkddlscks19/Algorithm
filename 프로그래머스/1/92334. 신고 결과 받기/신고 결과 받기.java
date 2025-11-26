08141281f4a4fd407fb1051f005e77825c23a988
ghkddlscks19 <84233046+ghkddlscks19@users.noreply.github.com>
2025-11-26 16:51:50 +0900

    [level 1] Title: 신고 결과 받기, Time: 179.55 ms, Memory: 180 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -1,46 +1,48 @@
 import java.util.*;
 class Solution {
     public int[] solution(String[] id_list, String[] report, int k) {
-        // 신고 해시맵 (신고 당한 사람, 신고한 사람(중복 X))
-        HashMap<String, HashSet<String>> reportMap = new HashMap<>();
-        
-        // 각 유저 별 신고 당한 횟수
-        HashMap<String, Integer> countMap = new HashMap<>();
-        
-        // 신고 해시맵에 넣어 주기
-        for(String rep: report) {
-            String tmp[] = rep.split(" ");
-            String reportUser = tmp[0]; // 신고한 사람
-            String reportedUser = tmp[1]; // 신고 당한 사람
-            
-            // 현재 해시맵에 들어있지 않다면 초기화 해주기
-            if(!reportMap.containsKey(reportedUser)) {
-                reportMap.put(reportedUser, new HashSet<>());
-            }
-             
-            reportMap.get(reportedUser).add(reportUser);
-            
+        // 신고 당한사람, 신고 한사람(중복x)
+        Map<String, Set<String>> reportMap = new HashMap<>();
+       
+        for(String id: id_list) {
+            reportMap.put(id, new HashSet<>());
         }
         
-        for(String reportedUser: reportMap.keySet()) {
-            // 신고를 k건 이상 받았을 때 정지 사실 메일로 발송하기
-            if(reportMap.get(reportedUser).size() >= k) {
-                
-                List<String> list = new ArrayList<>(reportMap.get(reportedUser));
-                
-                for(String str: list) {
-                    countMap.put(str, countMap.getOrDefault(str, 0) + 1);
-                }
+        // 신고 맵에 값 넣기
+        for(String s: report) {
+            String[] input = s.split(" ");
+            String reporter = input[0];
+            String reported = input[1];
+            
+            reportMap.get(reported).add(reporter);
+        }
+        
+        // k번 이상 신고 당한 사람 찾기
+        Set<String> reportedSet = new HashSet<>();
+        for(String key: reportMap.keySet()) {
+            if(reportMap.get(key).size() >= k) {
+                reportedSet.add(key);
             }
         }
         
+        // 메일 받을 횟수 계산
+        Map<String, Integer> mailCountMap = new HashMap<>();
+        for(String id: id_list) {
+            mailCountMap.put(id, 0);
+        }
         
+        for(String reported: reportedSet) {
+            for(String reporter: reportMap.get(reported)) {
+                mailCountMap.put(reporter, mailCountMap.get(reporter) + 1);
+            }
+        }
+        
+        // id_list 순서대로 담기
         int[] answer = new int[id_list.length];
-        
         for(int i =0; i<id_list.length; i++) {
-            answer[i] = countMap.getOrDefault(id_list[i], 0);
+            answer[i] = mailCountMap.get(id_list[i]);
         }
-        
+       
         return answer;
     }
 }
