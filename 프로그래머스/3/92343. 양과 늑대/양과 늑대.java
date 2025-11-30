03590a3e5b0433cbee40b76b35838dfa1a04f53c
ghkddlscks19 <84233046+ghkddlscks19@users.noreply.github.com>
2025-11-30 18:00:40 +0900

    [level 3] Title: 양과 늑대, Time: 3.88 ms, Memory: 93.5 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
@@ -1,66 +1,50 @@
 import java.util.*;
 class Solution {
-    static ArrayList<Integer> tree[];
-    static class Info{
-        int node;
-        int sheep;
-        int wolf;
-        List<Integer> visited;
-        public Info(int node, int sheep, int wolf, List<Integer> visited){
-            this.node = node;
-            this.sheep = sheep;
-            this.wolf = wolf;
-            this.visited = visited;
-        }
-    }
-    //트리 생성
-    public void makeTree(int[] info, int[][] edges){
-        tree = new ArrayList[info.length];
-        
-        for(int i =0; i<info.length; i++){
-            tree[i] = new ArrayList<>();
-        }
-        
-        for(int[] edge : edges){
-            tree[edge[0]].add(edge[1]);
-        }
-    }
+    List<List<Integer>> tree;
+    int answer = 0;
+    
     public int solution(int[] info, int[][] edges) {
-        makeTree(info, edges);
-        //양의 최대 수
-        int answer = 0;
-        //BFS로 순회할 큐 생성
-        ArrayDeque<Info> queue = new ArrayDeque<>();       
+        // 트리 형태로 만들기
+        tree = new ArrayList<>();
+        for(int i =0; i<info.length; i++) {
+            tree.add(new ArrayList<>());
+        }
+        for(int[] edge: edges) {
+            tree.get(edge[0]).add(edge[1]);
+        }
         
-        queue.add(new Info(0, 1, 0, new ArrayList<>()));
+        // 현재 갈 수 있는 곳 관리
+        List<Integer> canVisit = new ArrayList<>();
+        canVisit.add(0); 
         
-        while(!queue.isEmpty()){
-            Info curr = queue.poll();
-            answer = Math.max(answer, curr.sheep);
-            //현재 방문 가능한 노드 넣기
-            curr.visited.addAll(tree[curr.node]);
-            
-            for(int next : curr.visited){
-                //현재 방문 가능한 곳 복사
-                List<Integer> list = new ArrayList<>(curr.visited);
-                //next는 방문 한거니까 제거
-                list.remove(Integer.valueOf(next));
-                
-                System.out.println(list.toString());
-                
-                //늑대일 때
-                if(info[next] == 1){
-                    //양의 수는 늑대+1보다 클 때만 방문
-                    if(curr.sheep > curr.wolf+1){
-                        queue.add(new Info(next, curr.sheep, curr.wolf+1, list));
-                    }
-                }else{
-                    queue.add(new Info(next, curr.sheep+1, curr.wolf, list));   
-                }
-            }
-            
-        }      
+        dfs(0, 0, canVisit, info);
         
         return answer;
+    }
+    
+    void dfs(int sheep, int wolf, List<Integer> canVisit, int[] info) {
+        answer = Math.max(sheep, answer);
+        
+        for(int node: canVisit) {
+            // 노드 방문 후 양, 늑대 계산
+            int newSheep = sheep;
+            int newWolf = wolf;
+            if(info[node] == 0) {
+                newSheep++;
+            } else {
+                newWolf++;
+            }
+            
+            // 늑대가 양보다 많으면 안됨
+            if(newWolf >= newSheep) continue;
+            
+            // 현재 새롭게 방문할 수 있는 리스트 만들기
+            List<Integer> newCanVisit = new ArrayList<>(canVisit);
+            newCanVisit.remove(Integer.valueOf(node));
+            newCanVisit.addAll(tree.get(node));
+            
+            // 다음 탐색
+            dfs(newSheep, newWolf, newCanVisit, info);
+        }
     }
 }
