60f43ed5f5fcc67b3c442600ba67d1db23b2b728
ghkddlscks19 <84233046+ghkddlscks19@users.noreply.github.com>
2025-12-17 21:23:19 +0900

    [Silver I] Title: 관중석, Time: 244 ms, Memory: 18336 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/10166. 관중석/관중석.java
+++ 백준/Silver/10166. 관중석/관중석.java
@@ -9,6 +9,7 @@ public class Main{
     int D1 = Integer.parseInt(st.nextToken());
     int D2 = Integer.parseInt(st.nextToken());
 
+    boolean[][] visited = new boolean[2001][2001];
     // 각도가 같으면 가려져 있다
     // 0/3, 1/3, 2/3
     // 0/4, 1/4, 2/4, 3/4
@@ -16,18 +17,22 @@ public class Main{
     // 0/6, 1/6, 2/6, 3/6, 4/6, 5/6
     // 같은 기약분수끼리는 겹침 안겹치는거 세면 정답
 
-    Set<String> hs = new HashSet<>();
+    int answer = 0;
     for(int i =D1; i<=D2; i++) {
       for(int j =0; j<i; j++) {
         // 최대 공약수로 나눠줘야 기약분수가 됨
         int gcd = gcd(i, j);
         int bunja = j / gcd;
         int bunmo = i / gcd;
-        hs.add(String.valueOf(bunja + "/" + bunmo));
+
+        if(!visited[bunmo][bunja]) {
+            visited[bunmo][bunja] = true;
+            answer++;
+        }
       }
     }
 
-    bw.write(hs.size() + "");
+    bw.write(answer + "");
     bw.flush();
     bw.close();
     
