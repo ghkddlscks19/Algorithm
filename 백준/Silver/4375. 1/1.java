539bb6e8d57f25ffb6d74399e1c4009a210c6c7d
ghkddlscks19 <84233046+ghkddlscks19@users.noreply.github.com>
2025-12-24 14:09:40 +0900

    [Silver III] Title: 1, Time: 112 ms, Memory: 14560 KB -BaekjoonHub

--- 백준/Silver/4375. 1/1.java
+++ 백준/Silver/4375. 1/1.java
@@ -4,28 +4,27 @@ public class Main{
   public static void main(String args[]) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
+    
     String input = "";
-    int n, one, length;
-    while((input = br.readLine()) != null) {
-      n = Integer.parseInt(input);
-      one = 1;
-      length = 1;
 
-      // 1이면 답은 1
-      if(n == 1) {
-        bw.write("1\n");
-      } else {
-        while(one != 0) {
-          one = (one * 10 + 1) % n; // 1 -> 11 -> 111 (이전 수 * 10 + 1)
-          length++;
+    while((input = br.readLine()) != null) {
+      int n = Integer.parseInt(input);
+      int i = 0;
+      int length = 0;
+      while(true) {
+        i = (i * 10 + 1) % n;
+        length++;
+  
+        if(i % n == 0) {
+          bw.write(length + "\n");
+          break;
         }
-        bw.write(length + "\n");
       }
+     
     }
+
     bw.flush();
     bw.close();
+    
   }
 }
-
-
