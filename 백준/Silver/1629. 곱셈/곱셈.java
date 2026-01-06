fe11a9e30982762a4dd915c1520e7dab21457cae
ghkddlscks19 <84233046+ghkddlscks19@users.noreply.github.com>
2026-01-06 15:03:43 +0900

    [Silver I] Title: 곱셈, Time: 100 ms, Memory: 14228 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.java
+++ 백준/Silver/1629. 곱셈/곱셈.java
@@ -6,20 +6,28 @@ public class Main{
     BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
     StringTokenizer st = new StringTokenizer(br.readLine());
-    long A = Integer.parseInt(st.nextToken());
-    long B = Integer.parseInt(st.nextToken());
-    long C = Integer.parseInt(st.nextToken());
+    int A = Integer.parseInt(st.nextToken());
+    int B = Integer.parseInt(st.nextToken());
+    int C = Integer.parseInt(st.nextToken());
 
-    long result = recursive(A, B, C);
-    bw.write(result + "");
+    long answer = dfs(A, B, C);
+    bw.write(answer + "");
     bw.flush();
     bw.close();
   }
-  static long recursive(long A, long B, long C) {
-    if(B == 1) return A % C;
-    long val = recursive(A, B/2, C);
-    val = val * val % C;
-    if(B % 2 == 0) return val;
-    return val * A % C;
+
+  static long dfs(int A, int B, int C) {
+    // 지수가 1이 되면 종료
+    if(B == 1) {
+      return A % C;
+    }
+    
+    long half = dfs(A, B/2, C);
+
+    if(B % 2 == 0) {
+      return half * half % C;
+    } else {
+      return (half * half % C) * A % C;
+    }
   }
 }
