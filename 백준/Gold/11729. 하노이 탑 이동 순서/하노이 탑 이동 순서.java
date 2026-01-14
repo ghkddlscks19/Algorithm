bce7e2b4677390805dfede3888419de7cb474acd
ghkddlscks19 <84233046+ghkddlscks19@users.noreply.github.com>
2026-01-14 10:54:04 +0900

    [Gold V] Title: 하노이 탑 이동 순서, Time: 388 ms, Memory: 63108 KB -BaekjoonHub

--- 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.java
+++ 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.java
@@ -1,35 +1,32 @@
 import java.util.*;
 import java.io.*;
 public class Main{
+  static int cnt = 0;
   static StringBuilder sb = new StringBuilder();
-  static int count = 0;
   public static void main(String args[]) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-    int N = Integer.parseInt(br.readLine()); 
-    hanoi(N, 1, 2, 3);
-
-    bw.write(count + "\n");
+    int N = Integer.parseInt(br.readLine());
+    hanoi(1, 2, 3, N);
+    bw.write(cnt + "\n");
     bw.write(sb.toString());
     bw.flush();
     bw.close();
   }
 
-  // N: 원판 갯수, start: 시작지, via: 경유지, end: 도착지
-  static void hanoi(int N, int start, int via, int end) {
-    if(N == 1) {
+  static void hanoi(int start, int mid, int end, int n) {
+    // n이 1이면 출력
+    if(n == 1) {
+      cnt++;
       sb.append(start + " " + end + "\n");
-      count++;
       return;
     }
-
-    // 1. N-1개의 원판을 시작지 -> 경유지
-    hanoi(N-1, start, end, via);
-    // 2. 가장 큰 원판을 시작지에서 목적지로 옮긴다
-    sb.append(start + " " + end + "\n");
-    count++;
-    // 3. N-1개의 원판을 경유지 -> 도착지
-    hanoi(N-1, via, start, end);
+    // 1. n-1개 start -> mid로 이동 
+    hanoi(start, end, mid, n-1);
+    // 2. 1개를 start -> end로 이동 후 출력
+    hanoi(start, mid, end, 1);
+    // 3. n-1개 mid -> end로 이동
+    hanoi(mid, start, end, n-1);
   }
 }
